8da62af1cec9d849baa32894779215e436318b71
Oscar Andres Hernandez Ariza <oahernandez31@misena.edu.co>
2016-01-29 10:25:40 -0500

    update1

---
+++ Hola mundo/src/hola/mundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hola mundo");
+    }
+    
+}
